070ef8d825447c975472402c32da32247fd2b602
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-10-29 20:21:03 +0000

    ignore file

---
+++ avatar/download_avatars.js
@@ -0,0 +1,20 @@
+var request = require('request');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'nohkes',
+      'Authorization': 'token'
+    }
+}
+
+request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
