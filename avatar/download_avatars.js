79376698d8a69f6d914015b84eba364a15585ac4
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-10-29 21:34:45 +0000

    pull the usernames

--- avatar/download_avatars.js
+++ avatar/download_avatars.js
@@ -1,20 +1,32 @@
 var request = require('request');
+var getToken = require('./secrets.js'); 
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'nohkes',
-      'Authorization': 'token'
+      'User-Agent': 'request',
+      'Authorization': 'token ' + getToken.GITHUB_TOKEN
     }
 }
 
 request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body)
+    var arr = [];
+    for(var i = 0; i < data.length; i++){
+    	arr.push(data[i].avatar_url)
+    }
+    cb(err, arr);
   });
 }
+
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+// + getToken.GITHUB_TOKEN
